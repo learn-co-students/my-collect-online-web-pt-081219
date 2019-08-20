a6b18ae9b8fafb4135ad40eabc1018eddbe3f72c
Steven Laff <steve.laff@gmail.com>
2019-08-20 19:23:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  collection = []
+  if block_given?
+    i = 0
+    while i < array.size
+      yield array[i]
+      collection << array[i]
+      i += 1
+    end
+  else
+    puts "No block given."
+  end
+  collection
+end
