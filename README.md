71ab8f696d8c825b8bb8dbd503705f00a830d43f
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-22 17:23:42 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# My `#collect`
+I# My `#collect`
 
 ## Objectives
 
--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-
-
+def my_collect(array) 
+  i = 0 
+  new_array = []
+  while i < array.length
+  new_array << yield(array[i])
+   i += 1 
+end
+new_array
+end 
+  
+# my_collect(languages) do |language| 
+#   language.upcase
+# end
+# my_collect(languages) do |language|
+#       language.upcase
+#     end
