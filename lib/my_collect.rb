0b06dfc0f6c9735a705972bfefbc47f649915a61
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-22 02:46:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+i = 0 
+grouping = []
+while i < array.length 
+  grouping << yield(array[i])
+  i += 1
+end
+grouping
+end
+#my_collect(languages) do |language|
+         #language.upcase
+#my_collect(students) do |student|
+         #student.split(" ").first
