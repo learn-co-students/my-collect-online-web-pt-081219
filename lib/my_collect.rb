1639ceff3f17b7210f72bc027fa33425fa27ae44
Rachel T <rachmaybe@gmail.com>
2019-08-23 14:38:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+# def my_collect(empty_array) 
+#   array = ()
+# end 
 
-
+def my_collect(array)
+  counter = 0
+  result = []
+   while counter < array.size
+    result << yield(array[counter])
+    counter += 1
+  end
+  result
+end
